76c80a2ad16e3f353e752262a88f23f3ec55496e
sheikh ariful <sheikharifulislam770@gmail.com>
2022-07-06 02:07:52 +0600

    add find max number,find number,sort array,find min number and etc functionally

--- array.c
+++ array.c
@@ -10,6 +10,7 @@ int main() {
     int length = 0;
     int input;
     int value,index;
+    int min,max,sum,average,search,sort;
 
     do {
         printf("Menu: \n");
@@ -19,7 +20,13 @@ int main() {
         printf("3. Edit\n");
         printf("4. Delete\n");
         printf("5. Display\n");
-        printf("6. Clear\n");
+        printf("6. Min\n");
+        printf("7. Max\n");
+        printf("8. Sum\n");
+        printf("9. Average\n");
+        printf("10. Search\n");
+        printf("11. Sort\n");
+        printf("12. Clear\n");
         printf("0. Exit\n");
 
         printf("----------------\n");
@@ -27,10 +34,6 @@ int main() {
         printf("Enter A Menu: ");
         scanf("%d",&input);
         system("cls");
-        //printf("------- Result -------\n");
-        //printf("You Have Selected Menu No %d\n",input);
-        //printf("------- Result End -------\n\n");
-
         printf("------- Result -------\n");
 
         switch(input) {
@@ -40,7 +43,7 @@ int main() {
             break;
 
         case 1:
-            //add operation
+            // Add operation
             printf("Please Enter an Interger Value: ");
             scanf("%d",&value);
 
@@ -51,27 +54,47 @@ int main() {
             break;
 
         case 2:
-            // insert operation
-            printf("Please Enter A Interget Value: ");
+            // Insert operation
+            printf("Please Enter A Interger Value: ");
             scanf("%d",&value);
-            printf("Please Enter an Index Between 0 -%d: ",length);
+            printf("Please Enter an Index Between 0 - %d: ",length);
             scanf("%d",&index);
             for(int i = length; i > index; i--) {
                 data[i] = data[i - 1];
             }
             data[index] = value;
             length++;
-            printf("%d insert index %d",value,index);
+            printf("%d insert index %d\n",value,index);
             break;
 
         case 3:
+
+            // Edit Operation
+            printf("Please Enter an Index Between 0 - %d: ",length-1);
+            scanf("%d",&index);
+            printf("Please Enter A Interger Value: ");
+            scanf("%d",&value);
+            data[index] = value;
+            printf("%d updated at index %d\n",value,index);
+
             break;
 
         case 4:
+            // Delete operation
+            printf("Please Enter an Index Between 0 - %d: ",length-1);
+            scanf("%d",&index);
+            int dv = data[index];
+            for(int i = index; i < length; i++) {
+                data[i] = data[i + 1];
+            }
+            data[length] = 0;
+            length--;
+
+            printf("%d delete from %d th index\n",dv,index);
             break;
 
         case 5:
-            // display items
+            // Display items
             if(!length) {
                 printf("There No Data\n");
             }
@@ -85,10 +108,99 @@ int main() {
             break;
 
         case 6:
-            // clear data
+            // Find min value in the array
+            min = data[0];
+            for(int i = 0; i < length; i++) {
+                if(data[i] < min) {
+                    min = data[i];
+                }
+            }
+
+            printf("%d is min value in the array\n",min);
+            break;
+
+        case 7:
+            // Find max value in the array
+            max = data[0];
+            for(int i = 0; i < length; i++) {
+                if(data[i] > max) {
+                    min = data[i];
+                }
+            }
+
+            printf("%d is max value in the array\n",min);
+            break;
+
+        case 8:
+            // Sum operation
+            sum = 0;
+            for(int i = 0; i < length; i++) {
+                sum += data[i];
+            }
+
+            printf("total sum is: %d\n",sum);
+            break;
+
+        case 9:
+            // Average operation
+            sum = 0;
+            for(int i = 0; i < length; i++) {
+                sum += data[i];
+            }
+
+            average = sum / length;
+
+            printf("Average Value is %d\n",average);
+
+            break;
+
+        case 10:
+            // Search item from array
+            printf("Please Enter Your Search Item: ");
+            scanf("%d",&value);
+            int i;
+            int count = 0;
+            for(i = 0; i < length; i++) {
+                if(data[i] == value) {
+                    printf("%d is found in the array. It is %d th element in the array\n",data[i],i);
+                }
+                else {
+                    count++;
+                }
+            }
+
+            if(count == i) {
+                printf("%d is not found in the array\n",value);
+            }
+
+            break;
+
+        case 11:
+            // sort array
+            for(int i = 0; i < length; i++) {
+                for(int j = i; j < length; j++) {
+                    if(data[i] > data[j]) {
+                        int temp = data[i];
+                        data[i] = data[j];
+                        data[j] = temp;
+                    }
+                }
+            }
+
+            for(int i = 0; i < length; i++) {
+                printf("%d ,",data[i]);
+            }
+
+            printf("\n");
+
+            break;
+
+        case 12:
+            // Clear array
             data[0];
             length = 0;
             printf("Data Clear\n");
+
             break;
 
         default:
@@ -103,6 +215,7 @@ int main() {
 
 
     system("cls");
+    printf("Thanks For Using Our Application. Goo Buy");
 
 
     return 0;
